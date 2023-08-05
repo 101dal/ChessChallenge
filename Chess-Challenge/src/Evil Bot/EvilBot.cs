37038128e4b8f00e71998c23cece47055770674b
BuildTools <unconfigured@null.spigotmc.org>
2023-08-05 16:07:14 +0200

    v0 - Modified EvilBot + implement basic negamax with evaluation function

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,69 +1,230 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChessChallenge.Example
 {
     public class EvilBot : IChessBot
     {
-        //                     .  P    K    B    R    Q    K
-        int[] kPieceValues = { 0, 100, 300, 310, 500, 900, 10000 };
-        int kMassiveNum = 99999999;
 
-        int mDepth;
-        Move mBestMove;
+
+        private static readonly Random RandomGenerator = new Random();
+        // Null = 0, Pawn = 100, Knight = 300, Bishop = 320, Rook = 500, Queen = 900, King = 10000
+        private static readonly int[] PieceValues = { 0, 100, 300, 320, 500, 900, 10000 };
+
+        private int positionsEvaluated = 0;
+        private Dictionary<ulong, TranspositionEntry> transpositionTable = new Dictionary<ulong, TranspositionEntry>();
 
         public Move Think(Board board, Timer timer)
         {
-            Move[] legalMoves = board.GetLegalMoves();
-            mDepth = 3;
+            Move[] allMoves = OrderMoves(board);
+            bool isWhite = board.IsWhiteToMove;
+            int bestScore = (isWhite) ? int.MinValue : int.MaxValue;
+            int depth = 3;
+            Move bestMove = allMoves[0];
 
-            EvaluateBoardNegaMax(board, mDepth, -kMassiveNum, kMassiveNum, board.IsWhiteToMove ? 1 : -1);
-
-            return mBestMove;
-        }
-
-        int EvaluateBoardNegaMax(Board board, int depth, int alpha, int beta, int color)
-        {
-            Move[] legalMoves;
-
-            if (board.IsDraw())
-                return 0;
-
-            if (depth == 0 || (legalMoves = board.GetLegalMoves()).Length == 0)
+            for (int d = 1; d <= depth; d++)
             {
-                // EVALUATE
-                int sum = 0;
+                positionsEvaluated = 0;
+                foreach (Move move in allMoves)
+                {
+                    board.MakeMove(move);
+                    int score = MinimaxWithTransposition(board, d, int.MinValue, int.MaxValue, !isWhite);
+                    board.UndoMove(move);
 
-                if (board.IsInCheckmate())
-                    return -9999999;
-
-                for (int i = 0; ++i < 7;)
-                    sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * kPieceValues[i];
-                // EVALUATE
-
-                return color * sum;
+                    if ((isWhite && score > bestScore) || (!isWhite && score < bestScore))
+                    {
+                        bestScore = score;
+                        bestMove = move;
+                    }
+                }
             }
 
-            // TREE SEARCH
-            int recordEval = int.MinValue;
+            return bestMove;
+        }
+
+        private int MinimaxWithTransposition(Board board, int depth, int alpha, int beta, bool maximizingPlayer)
+        {
+            if (depth <= 0 || board.IsDraw() || board.IsInCheckmate())
+            {
+                positionsEvaluated++;
+                return Evaluate(board, maximizingPlayer);
+            }
+
+            ulong zobristKey = board.ZobristKey;
+            if (transpositionTable.TryGetValue(zobristKey, out TranspositionEntry? entry) && entry != null && entry.Depth >= depth)
+            {
+                if (entry.EntryType == TranspositionEntryType.Exact)
+                    return entry.Score;
+                if (entry.EntryType == TranspositionEntryType.LowerBound)
+                    alpha = Math.Max(alpha, entry.Score);
+                if (entry.EntryType == TranspositionEntryType.UpperBound)
+                    beta = Math.Min(beta, entry.Score);
+
+                if (alpha >= beta)
+                    return entry.Score;
+            }
+
+            if (maximizingPlayer)
+            {
+                int maxScore = int.MinValue;
+                foreach (Move move in OrderMoves(board))
+                {
+                    board.MakeMove(move);
+                    int score = MinimaxWithTransposition(board, depth - 1, alpha, beta, false);
+                    board.UndoMove(move);
+                    maxScore = Math.Max(maxScore, score);
+                    alpha = Math.Max(alpha, maxScore);
+                    if (beta <= alpha)
+                    {
+                        break; // Beta cutoff
+                    }
+                }
+
+                TranspositionEntryType entryType = (maxScore <= alpha) ? TranspositionEntryType.UpperBound : TranspositionEntryType.Exact;
+                transpositionTable[zobristKey] = new TranspositionEntry(entryType, depth, maxScore);
+                return maxScore;
+            }
+            else
+            {
+                int minScore = int.MaxValue;
+                foreach (Move move in OrderMoves(board))
+                {
+                    board.MakeMove(move);
+                    int score = MinimaxWithTransposition(board, depth - 1, alpha, beta, true);
+                    board.UndoMove(move);
+                    minScore = Math.Min(minScore, score);
+                    beta = Math.Min(beta, minScore);
+                    if (beta <= alpha)
+                    {
+                        break; // Alpha cutoff
+                    }
+                }
+
+                TranspositionEntryType entryType = (minScore >= beta) ? TranspositionEntryType.LowerBound : TranspositionEntryType.Exact;
+                transpositionTable[zobristKey] = new TranspositionEntry(entryType, depth, minScore);
+                return minScore;
+            }
+        }
+
+        private int Evaluate(Board board, bool isWhiteToMove)
+        {
+            int evaluation = 0;
+
+            // Evaluate the pieces value
+            evaluation += EvaluatePieces(board);
+
+            //ABSOLUTE RULES(integer limits)//
+            if (board.IsInCheckmate())
+            {
+                if (isWhiteToMove)
+                {
+                    evaluation = int.MinValue;
+                }
+                else
+                {
+                    evaluation = int.MaxValue;
+                }
+            }
+            if (board.IsDraw())
+            {
+                return 0;
+            }
+
+
+            return evaluation;
+        }
+
+        private int EvaluatePieces(Board board)
+        {
+            int evaluation = 0;
+
+            foreach (PieceList pieces in board.GetAllPieceLists())
+            {
+                foreach (Piece piece in pieces)
+                {
+                    int value = PieceValues[(int)piece.PieceType];
+                    if (piece.IsWhite)
+                        evaluation += value;
+                    else
+                        evaluation -= value;
+                }
+            }
+
+            return evaluation;
+        }
+
+        private Move[] OrderMoves(Board board)
+        {
+            Move[] legalMoves = board.GetLegalMoves();
+            List<KeyValuePair<int, Move>> finalMoves = new List<KeyValuePair<int, Move>>();
+
             foreach (Move move in legalMoves)
             {
+                int score = 0;
+
+                //BONUSES//
+                if (move.IsCapture)
+                {
+                    score += PieceValues[(int)move.CapturePieceType] - PieceValues[(int)move.MovePieceType];
+                }
+                if (move.IsCastles)
+                {
+                    score += 10000;
+                }
+
+                //MALUSES//
+                if (PieceValues[(int)move.CapturePieceType] < PieceValues[(int)move.MovePieceType])
+                {
+                    score -= PieceValues[(int)move.MovePieceType];
+                }
+
+                //ABSOLUTE RULES (intiger limits)//
+
                 board.MakeMove(move);
-                int evaluation = -EvaluateBoardNegaMax(board, depth - 1, -beta, -alpha, -color);
+
+                //If the board is in checkmate then I prioritize this move
+                if (board.IsInCheckmate())
+                {
+                    score = int.MaxValue;
+                }
                 board.UndoMove(move);
 
-                if (recordEval < evaluation)
-                {
-                    recordEval = evaluation;
-                    if (depth == mDepth)
-                        mBestMove = move;
-                }
-                alpha = Math.Max(alpha, recordEval);
-                if (alpha >= beta) break;
-            }
-            // TREE SEARCH
 
-            return recordEval;
+                // Add the move with its value to the finalMoves list
+                finalMoves.Add(new KeyValuePair<int, Move>(score, move));
+            }
+
+            // Sort the list to get ordered from the highest score to lowest
+            finalMoves.Sort((a, b) => b.Key.CompareTo(a.Key));
+
+            // Extract the sorted moves from the list
+            Move[] sortedMoves = finalMoves.Select(entry => entry.Value).ToArray();
+
+            return sortedMoves;
         }
+    }
+
+    public enum TranspositionEntryType
+    {
+        Exact,
+        LowerBound,
+        UpperBound
+    }
+
+    public class TranspositionEntry
+    {
+        public TranspositionEntryType EntryType { get; }
+        public int Depth { get; }
+        public int Score { get; }
+
+        public TranspositionEntry(TranspositionEntryType entryType, int depth, int score)
+        {
+            EntryType = entryType;
+            Depth = depth;
+            Score = score;
+        }
+
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,49 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
 using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    readonly int infinite = 100000;
-    readonly int depth = 3;
-
+    private int positions = 0; //#DEBUG
+    private readonly int infinity = 1000000;
     public Move Think(Board board, Timer timer)
     {
-        Move bestMove = Move.NullMove;
-        int max = -infinite;
+        int depth = 3; // You can adjust the search depth as needed.
 
-        foreach(Move move in board.GetLegalMoves()) {
+        Move bestMove = Move.NullMove;
+        int bestScore = -infinity;
+        int alpha = -infinity;
+        int beta = infinity;
+
+        foreach (var move in board.GetLegalMoves())
+        {
             board.MakeMove(move);
-            int eval = Negamax(board, depth - 1);
+            int score = -Negamax(board, depth - 1, -beta, -alpha);
             board.UndoMove(move);
-            if (eval > max) {
-                max = eval;
+
+            if (score > bestScore)
+            {
+                bestScore = score;
                 bestMove = move;
             }
+
+            alpha = Math.Max(alpha, score);
         }
+
+        Console.WriteLine($"{positions} PV");
+
         return bestMove;
     }
 
-    int Negamax(Board board, int depth)
+    private int Negamax(Board board, int depth, int alpha, int beta)
     {
-        if (depth == 0) return Evaluation(board);
-        int max = -infinite;
-        foreach (Move move in board.GetLegalMoves())
+        if(board.IsInCheckmate() || board.IsDraw()) {
+            return Evaluate(board, depth);
+        }
+        if (depth == 0)
+            return Evaluate(board);
+
+        int bestScore = -infinity;
+
+        foreach (var move in board.GetLegalMoves())
         {
             board.MakeMove(move);
-            int score = -Negamax(board, depth - 1);
+            int score = -Negamax(board, depth - 1, -beta, -alpha);
             board.UndoMove(move);
-            if (score > max)
-            {
-                max = score;
-            }
+
+            if (score > bestScore)
+                bestScore = score;
+
+            alpha = Math.Max(alpha, score);
+
+            if (alpha >= beta)
+                break;
         }
-        return max;
+
+        return bestScore;
     }
 
-    // Positive if white and negative if black
-    int Evaluation(Board board)
+    private readonly int[] pieceValues = { 0, 100, 320, 330, 500, 900, 10000 };
+    private int Evaluate(Board board, int depth = 0)
     {
-        return 0;
+        positions++; //#DEBUG
+        // Equal / draw
+        int evaluation = 0;
+        // Add your evaluation logic here.
+        // You can assign scores based on piece values, piece positions, mobility, and other factors.
+        // Positive scores favor white, and negative scores favor black.
+        // For simplicity, this example returns a random score between -100 and 100.
+
+        //Piece evaluation
+        foreach (PieceList pieces in board.GetAllPieceLists())
+        {
+            foreach (Piece piece in pieces)
+            {
+                int score = pieceValues[(int)piece.PieceType];
+                evaluation += (piece.IsWhite) ? score : -score;
+            }
+        }
+
+        //Absolute rules
+        if (board.IsInCheckmate())
+        {
+            return board.IsWhiteToMove ? -infinity - depth : infinity + depth;
+        }
+        if (board.IsDraw()) return 0;
+        return evaluation;
     }
 }
