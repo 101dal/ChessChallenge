6dc5cae35066ce205baad994b06a4821ba4f8afd
BuildTools <unconfigured@null.spigotmc.org>
2023-08-05 23:06:24 +0200

    v2 - +200 elo over evil bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,58 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
     private int positions = 0; //#DEBUG
-    private readonly int infinity = 1000000;
+    private const int infinity = 1000000;
+    private const int minDepth = 3; // Minimum depth for iterative deepening
+    private const int maxDepth = 10; // Maximum depth for iterative deepening
+    private const int maxTime = 100; // Maximum time for iterative deepening (in ms)
+
     public Move Think(Board board, Timer timer)
     {
-        int depth = 3; // You can adjust the search depth as needed.
-
+        Move[] allMoves = OrderedMoves(board);
         Move bestMove = Move.NullMove;
         int bestScore = -infinity;
         int alpha = -infinity;
         int beta = infinity;
 
-        foreach (var move in board.GetLegalMoves())
+        for (int depth = minDepth; depth <= maxDepth; depth++)
         {
-            board.MakeMove(move);
-            int score = -Negamax(board, depth - 1, -beta, -alpha);
-            board.UndoMove(move);
-
-            if (score > bestScore)
+            foreach (var move in allMoves)
             {
-                bestScore = score;
-                bestMove = move;
+                board.MakeMove(move);
+                int color = board.IsWhiteToMove ? 1 : -1;
+                int score = -Negamax(board, depth - 1, -beta, -alpha, color);
+                board.UndoMove(move);
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = move;
+                }
+
+                alpha = Math.Max(alpha, score);
+
+                if (alpha >= beta)
+                    break;
             }
 
-            alpha = Math.Max(alpha, score);
+            if (timer.MillisecondsElapsedThisTurn >= maxTime)
+                break;
         }
 
-        Console.WriteLine($"{positions} PV");
+        Console.WriteLine($"{positions} PV with best score of {bestScore}"); //#DEBUG
 
-        return bestMove;
+        return bestMove == Move.NullMove ? allMoves[0] : bestMove;
     }
 
-    private int Negamax(Board board, int depth, int alpha, int beta)
+    private int Negamax(Board board, int depth, int alpha, int beta, int color)
     {
-        if(board.IsInCheckmate() || board.IsDraw()) {
-            return Evaluate(board, depth);
-        }
         if (depth == 0)
-            return Evaluate(board);
+            return Evaluate(board, depth) * color;
 
         int bestScore = -infinity;
 
-        foreach (var move in board.GetLegalMoves())
+        foreach (var move in OrderedMoves(board))
         {
             board.MakeMove(move);
-            int score = -Negamax(board, depth - 1, -beta, -alpha);
+            int score = -Negamax(board, depth - 1, -beta, -alpha, -color);
             board.UndoMove(move);
 
-            if (score > bestScore)
-                bestScore = score;
-
+            bestScore = Math.Max(bestScore, score);
             alpha = Math.Max(alpha, score);
 
             if (alpha >= beta)
@@ -62,33 +71,64 @@ public class MyBot : IChessBot
         return bestScore;
     }
 
-    private readonly int[] pieceValues = { 0, 100, 320, 330, 500, 900, 10000 };
-    private int Evaluate(Board board, int depth = 0)
+    private readonly int[] pieceValues = { 0, 100, 300, 320, 500, 900, 10000 };
+
+    private int Evaluate(Board board, int depth)
     {
         positions++; //#DEBUG
-        // Equal / draw
-        int evaluation = 0;
-        // Add your evaluation logic here.
-        // You can assign scores based on piece values, piece positions, mobility, and other factors.
-        // Positive scores favor white, and negative scores favor black.
-        // For simplicity, this example returns a random score between -100 and 100.
 
-        //Piece evaluation
+        if (board.IsInCheckmate())
+            return board.IsWhiteToMove ? -infinity - depth : infinity + depth;
+
+        if (board.IsDraw())
+            return 0;
+
+        int evaluation = 0;
         foreach (PieceList pieces in board.GetAllPieceLists())
         {
             foreach (Piece piece in pieces)
             {
                 int score = pieceValues[(int)piece.PieceType];
-                evaluation += (piece.IsWhite) ? score : -score;
+                evaluation += piece.IsWhite ? score : -score;
             }
         }
 
-        //Absolute rules
-        if (board.IsInCheckmate())
-        {
-            return board.IsWhiteToMove ? -infinity - depth : infinity + depth;
-        }
-        if (board.IsDraw()) return 0;
         return evaluation;
+    }
+
+    private Move[] OrderedMoves(Board board)
+    {
+        Move[] allMoves = board.GetLegalMoves();
+        int[] scores = new int[allMoves.Length];
+
+        for (int i = 0; i < allMoves.Length; i++)
+        {
+            int score = 0;
+            Move move = allMoves[i];
+
+            if (move.IsPromotion)
+                score += pieceValues[(int)move.PromotionPieceType];
+
+            if (move.IsCapture)
+                score = 1000 * pieceValues[(int)move.CapturePieceType] - pieceValues[(int)move.MovePieceType];
+
+            if (move.IsCastles)
+                score += 10000;
+
+            if (move.MovePieceType == PieceType.King)
+                score -= infinity;
+
+            board.MakeMove(move);
+            if (board.IsInCheckmate())
+                score = infinity;
+            board.UndoMove(move);
+
+            scores[i] = score;
+        }
+
+        Array.Sort(scores, allMoves);
+        Array.Reverse(allMoves);
+
+        return allMoves;
     }
 }
